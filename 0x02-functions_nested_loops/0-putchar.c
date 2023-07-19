256d339f97a91f9bc600929a1a2e49cba40566a3
Fatine-elhassouni <elhassouni.fatine@gmail.com>
2023-07-18 17:03:11 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
 #include "main.h"
+
 /**
- * main - Entry point
- * Description: prints '_putchar\n'
- * Return: Always 0 (success)
+ * main - Entry Point
+ * Description - Print _putchar
+ * Return: 0
  */
+
 int main(void)
 {
-char str[] = "_putchar";
-int i = 0;
+	char c[] = "_putchar\n";
+	int i = 0;
 
-while (str[i] != '\0')
+	while (c[i] != '\0')
 	{
-	char c = str[i];
-
-	_putchar(c);
-	i++;
+		_putchar(c[i]);
+		i++;
 	}
-return (0);
+
+	return (0);
 }
